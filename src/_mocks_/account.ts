5edeb3e0a44d7105253ed489d22239c6aa3b1a63
Pazy <pazymonet@gmail.com>
2025-07-17 10:13:00 +0100

    Turbo Commit

---
+++ src/_mocks_/account.ts
@@ -0,0 +1,11 @@
+import { IAccount } from '@/models';
+import Avatar from '@/assets/images/avatar_default.png';
+
+const account: IAccount = {
+    displayName: 'Paschal Igbeia',
+    email: 'pazymonet@gmail.com',
+    photoURL: Avatar,
+    role: 'admin'
+};
+
+export default account;
